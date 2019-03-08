f94cbd77c2ff9457e98a4f70171250befc2ce824
dZallo <danielzallo@gmail.com>
2019-03-08 20:29:03 +0100

    Se agrega a LocationList un parametro que recibe que es un listado de ciudades.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import LocationList from './components/LocationList';
 import './App.css';
 
+const cities=[
+  'Bilbao,ES',
+  'Madrid,ES',
+  'London,UK',
+  'Buenos Aires, ar'
 
+];
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>WEATHER APP</h1>
-          <LocationList></LocationList>
+          <LocationList cities={cities}></LocationList>
         </header>
       </div>
     );
--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherLocation from './../components/WeatherLocation';
 
-const LocationList = () =>{
+const LocationList = ({cities}) =>{
+    console.log(cities);
+
     return(
     <div>
         <WeatherLocation city="Bilbao, Es"></WeatherLocation>
         <WeatherLocation city="Malaga, Es"></WeatherLocation>
-        <WeatherLocation city="Barcelona, Es"></WeatherLocation>
+        <WeatherLocation city="London, Uk"></WeatherLocation>
     </div>
-
     )
+}
+
+LocationList.protoTypes = {
+    cities: PropTypes.array.isRequired,
 }
 export default LocationList;
