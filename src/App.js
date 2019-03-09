1844087685489e3dc8b2ee736e36fd907d9b1b72
dZallo <danielzallo@gmail.com>
2019-03-09 17:59:25 +0100

    Añadido la libreria react-flexbox-grid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import LocationList from './components/LocationList';
 import './App.css';
+import { MuiThemeProvider } from '@material-ui/core';
 
-const cities=[
+const cities = [
   'Bilbao,ES',
   'Madrid,ES',
   'London,UK',
@@ -10,18 +12,41 @@ const cities=[
 
 ];
 class App extends Component {
-  handleSelectionLocation = city =>{
+  handleSelectionLocation = city => {
     console.log("handleSelectionLocation App " + city);
   }
 
   render() {
     return (
-      <div className="App">
+      <MuiThemeProvider>
+        <Grid fluid>
+          <Row>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className="red">MD</div>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className="green">MD</div>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className="blue">MD</div>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <div className="yellow">MD</div>
+            </Col>
+          </Row>
+          </Grid>
+
+      </MuiThemeProvider>
+
+
+
+      /*<div className="App">
         <header className="App-header">
           <h1>WEATHER APP</h1>
           <LocationList cities={cities} onSelectedLocation={this.handleSelectionLocation}></LocationList>
         </header>
       </div>
+      */
     );
   }
 }
