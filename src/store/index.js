14bd2f34b57ac7d50ec4f1ddb4fc6f8b35ef9068
dZallo <danielzallo@gmail.com>
2019-03-26 23:37:38 +0100

    Reduce que modifica el estado

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,13 @@
 import { createStore } from 'redux';
+import { city } from './../reducers/city';
 
- // Sirve para que la extension de redux tools detecte el store
- export const store = createStore(() => { },
- window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+//Se le puede pasar un estado inicial
+const initialState = {
+    city: 'Bilbao,es'
+};
+
+
+// Sirve para que la extension de redux tools detecte el store
+export const store = createStore(city, initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
